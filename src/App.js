4a8867c0f8ad0f21879efdd2a3147601de7aaf9d
dejanV91 <dejanvukoicic91@gmail.com>
2022-11-11 18:28:19 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
 import List from "./List";
 function App() {
+  const [list, setList] = useState(<List />);
+  const [number, setNumber] = useState(5);
+
+  const changeEvenet = () => {
+    setList([]);
+    setNumber(0);
+  };
   return (
     <main>
       <section className="container">
-        <h3>5 Birthday Today</h3>
-        <List />
+        <h3>{number} Birthday Today</h3>
+        {list}
+        <button
+          className="btn"
+          onClick={() => {
+            changeEvenet();
+          }}
+        >
+          clear all
+        </button>
       </section>
     </main>
   );
